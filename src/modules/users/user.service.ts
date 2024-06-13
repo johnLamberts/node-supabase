d67cb25829b534a537e211b75e8ca50455e04883
johnLamberts <faithsacredoo3@gmail.com>
2024-06-13 15:31:51 +0800

    feat: add getUserById method

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,10 +1,10 @@
+import supabase from "@/config/supabase";
 import supabaseAdmin from "../../config/supabase-admin";
 import { Request, Response } from "express";
 export class UserService {
   async createUser(req: Request, res: Response) {
+    const { email, password, name } = req.body;
     try {
-      const { email, password, name } = req.body;
-
       const { data, error } = await supabaseAdmin.auth.admin.createUser({
         email: email,
         password: password,
@@ -29,7 +29,78 @@ export class UserService {
   }
 
   async getUsers(req: Request, res: Response) {
-    // const { data, error } = await supabase.auth.admin.listUsers();
-    // res.json(data);
+    try {
+      const { data, error } = await supabaseAdmin.auth.admin.listUsers();
+
+      if (error) {
+        res.status(parseInt(`${error.status}`, 10)).send({
+          error: error?.message,
+          code: error.code,
+        });
+      }
+
+      return res.json({
+        message: "Success",
+        content: data,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "An unexpected error occured" });
+    }
+  }
+
+  async getUserById(req: Request, res: Response) {
+    try {
+      const { data, error } = await supabaseAdmin.auth.admin.getUserById(
+        req.params.userId
+      );
+
+      console.log(req.params.userId);
+
+      if (error) {
+        res.status(parseInt(`${error.status}`, 10)).send({
+          error: error?.message,
+          code: error.code,
+        });
+      }
+
+      return res.json({
+        message: "Success",
+        content: data,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "An unexpected error occured" });
+    }
+  }
+
+  async updateUser(req: Request, res: Response) {
+    const { email, password } = req.body;
+    console.log(req.params.userId);
+    try {
+      // const { data, error } = await supabaseAdmin.auth.admin.updateUserById(
+      //   req.params.userId,
+      //   {
+      //     email: email,
+      //   }
+      // );
+
+      // console.log(error);
+
+      // if (error) {
+      //   res.status(error.status || 400).send({
+      //     error: error?.message,
+      //     code: error.code,
+      //   });
+      // }
+
+      return res.json({
+        message: "Success",
+        content: req.body,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "An unexpected error occured" });
+    }
   }
 }
