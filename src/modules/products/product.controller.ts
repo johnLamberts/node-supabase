f473142780a5ef5363e9ec567639fd4b8760994f
johnLamberts <faithsacredoo3@gmail.com>
2024-06-13 17:52:52 +0800

    fix: resolve import path issue using tsconfig-paths🫶🏼

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -1,11 +1,15 @@
-import BaseController from "../../common/base-controller";
+import API_VERSION from "@/utils/version";
+import BaseController from "@/common/base-controller";
 import { ProductService } from "./product.service";
 
 class ProductController extends BaseController {
   protected initializedRoutes(): void {
-    this.router.get("/all", this.service.getProducts);
-    this.router.put("/:id", this.service.updateProduct);
-    this.router.post("/add", this.service.createProduct);
+    this.router.get(`${API_VERSION}/product/all`, this.service.getProducts);
+    this.router.put(`${API_VERSION}/product/:id`, this.service.updateProduct);
+    this.router.post(
+      `${API_VERSION}/product/create_product`,
+      this.service.createProduct
+    );
   }
 }
 
